49634583f874165b08d600c7d04caa1b27349b63
Nattha45 <139251222+Nattha45@users.noreply.github.com>
2025-03-17 03:34:18 +0700

    sa

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ page = st.selectbox("Select a page", ["Main", "Home", "Neural Network", "Machine
 
 if page == "Main":
     # หน้า Main (Welcome Page)
-    st.title("🌟Welcome to Intelligent System🌟")
+    st.title("🌟Intelligent System🌟")
     st.write("""
         🤖 **Hello and Welcome!**  
         This is a basic knowledge center about intelligent systems. With basic AI models, please come and try it.! 🐶✨
